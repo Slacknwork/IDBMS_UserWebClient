a9dbe0c4d02145ce700fef6cea6c7ec503030e65
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-11-27 16:34:48 +0700

    add fetch api for Login and Register

--- app/[locale]/register/page.js
+++ app/[locale]/register/page.js
@@ -9,67 +9,113 @@ import Button from "@mui/material/Button";
 import { useRouter } from "next/navigation";
 import { Link } from "/navigation";
 import { FormControl, FormControlLabel, InputLabel, Radio, RadioGroup } from "@mui/material";
-import { Select } from "flowbite-react";
 import { MdOutlineRadioButtonChecked } from "react-icons/md";
+import DatePicker from 'react-datepicker'
+import { registerUser } from "../../../api/authenticationServices";
+import 'react-datepicker/dist/react-datepicker.css'
+
 
 const SignUpPage = (props) => {
   const router = useRouter();
 
-  const [value, setValue] = useState({
+  const [user, setUser] = useState({
     email: "",
+    bio: null,
     name: "",
     password: "",
     confirm_password: "",
     phone: "",
+    dateOfBirth: "",
     address: "",
-    language: "Vietnamese",
+    language: 1,
+    externalId: null,
   });
 
   const changeHandler = (e) => {
-    // setValue({ ...value, [e.target.name]: e.target.value });
-    // validator.showMessages();
     const fieldName = e.target.name;
     const fieldValue = e.target.value;
 
-    setValue({ ...value, [fieldName]: fieldValue });
-
-    // Validate the specific field
-    validator.showMessageFor(fieldName);
+    setUser({ ...user, [fieldName]: fieldValue });
   };
-
-  const [validator] = React.useState(
-    new SimpleReactValidator({
-      className: "errorMessage",
-    })
-  );
 
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
-
   const confirmMessage = () => {
-    if (value.password !== value.confirm_password) {
+    if (user.password !== user.confirm_password) {
       setConfirmPasswordError("Passwords must match!");
+      return false;
     } else {
       setConfirmPasswordError("");
+      return true;
     }
   };
 
-  const submitForm = (e) => {
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = emailRegex.test(user.email);
+
+    setEmailError(isEmailValid ? "" : "Invalid format for email address");
+
+    return isEmailValid;
+  };
+
+  const validatePhone = () => {
+    const phoneRegex = /^(\+\d{1,2}\s?)?(\d{9}|\d{10})$/;
+    const isPhoneValid = phoneRegex.test(user.phone);
+
+    setPhoneError(isPhoneValid ? "" : "Invalid format for phone number");
+
+    return isPhoneValid;
+  };
+
+  const handleDateChange = (date) => {
+    console.log(date)
+    setUser({
+      ...user,
+      dateOfBirth: date,
+    });
+  };
+
+  const submitForm = async (e) => {
     e.preventDefault();
-    if (validator.allValid()) {
-      setValue({
-        email: "",
-        full_name: "",
-        password: "",
-        confirm_password: "",
-      });
-      validator.hideMessages();
-      toast.success("Registration Complete successfully!");
-      router.push("/login");
+
+    const nonNullableFields = ['email', 'name', 'password', 'phone', 'address', 'language'];
+    if (nonNullableFields.some((field) => user[field] === null || user[field] === "")) {
+      toast.error("All fields must be filled out.");
     } else {
-      validator.showMessages();
-      toast.error("Empty field is not allowed!");
+      const isEmailValid = validateEmail();
+      const isPhoneValid = validatePhone();
+      const isPasswordConfirmed = confirmMessage();
+
+      if (isEmailValid && isPhoneValid && isPasswordConfirmed) {
+        const formattedUser = {
+          ...user,
+          dateOfBirth: user.dateOfBirth ? new Date(user.dateOfBirth).toISOString() : null,
+          language: user.language ? parseInt(user.language) : null,
+        };
+
+        try {
+          console.log(formattedUser);
+          const response = await registerUser(formattedUser);
+          console.log(response);
+          if (response.data != null) {
+            toast.success("Registration successful!");
+            // link
+          } else {
+            throw new Error("Registration failed");
+          }
+        } catch (error) {
+          console.error("Error registering user:", error);
+          toast.error("Error registering user");
+        }
+      } else {
+        toast.error("Please fix the validation errors before submitting.");
+      }
     }
   };
+
   return (
     <Grid className="loginWrapper">
       <Grid className="loginForm">
@@ -82,7 +128,7 @@ const SignUpPage = (props) => {
                 className="inputOutline"
                 fullWidth
                 placeholder="Enter full name"
-                value={value.name}
+                user={user.name}
                 variant="outlined"
                 name="name"
                 label="Name"
@@ -92,35 +138,35 @@ const SignUpPage = (props) => {
                 onBlur={(e) => changeHandler(e)}
                 onChange={(e) => changeHandler(e)}
               />
-              {validator.message(
-                "name",
-                value.name,
-                "required|alpha"
-              )}
             </Grid>
             <Grid item xs={12}>
               <TextField
                 className="inputOutline"
                 fullWidth
                 placeholder="Email"
-                value={value.email}
+                user={user.email}
                 variant="outlined"
                 name="email"
                 label="Email"
                 InputLabelProps={{
                   shrink: true,
                 }}
-                onBlur={(e) => changeHandler(e)}
+                onBlur={(e) => {
+                  changeHandler(e);
+                  validateEmail();
+                }}
                 onChange={(e) => changeHandler(e)}
               />
-              {validator.message("email", value.email, "required|email")}
+              {emailError && (
+                <span className="errorMessage">{emailError}</span>
+              )}
             </Grid>
             <Grid item xs={12}>
               <TextField
                 className="inputOutline"
                 fullWidth
                 placeholder="Enter password"
-                value={value.password}
+                user={user.password}
                 variant="outlined"
                 name="password"
                 label="Password"
@@ -131,14 +177,13 @@ const SignUpPage = (props) => {
                 onBlur={(e) => changeHandler(e)}
                 onChange={(e) => changeHandler(e)}
               />
-              {validator.message("password", value.password, "required")}
             </Grid>
             <Grid item xs={12}>
               <TextField
                 className="inputOutline"
                 fullWidth
                 placeholder="Confirm your password"
-                value={value.confirm_password}
+                user={user.confirm_password}
                 variant="outlined"
                 name="confirm_password"
                 label="Confirm Password"
@@ -154,11 +199,6 @@ const SignUpPage = (props) => {
                 }
                 onChange={(e) => changeHandler(e)}
               />
-              {validator.message(
-                "confirm_password",
-                value.confirm_password,
-                "required"
-              )}
               {confirmPasswordError && (
                 <span className="errorMessage">{confirmPasswordError}</span>
               )}
@@ -168,7 +208,7 @@ const SignUpPage = (props) => {
                 className="inputOutline"
                 fullWidth
                 placeholder="Enter address"
-                value={value.address}
+                user={user.address}
                 variant="outlined"
                 name="address"
                 label="Address"
@@ -178,43 +218,43 @@ const SignUpPage = (props) => {
                 onBlur={(e) => changeHandler(e)}
                 onChange={(e) => changeHandler(e)}
               />
-              {validator.message(
-                "address",
-                value.address,
-                "required"
-              )}
             </Grid>
             <Grid item xs={12}>
               <TextField
                 className="inputOutline"
                 fullWidth
-                placeholder="Enter phone"
-                value={value.phone}
+                placeholder="+84 779054212 or 0779054212"
+                user={user.phone}
                 variant="outlined"
                 name="phone"
                 label="Phone"
                 InputLabelProps={{
                   shrink: true,
                 }}
-                onBlur={(e) => changeHandler(e)}
+                onBlur={(e) => {
+                  changeHandler(e);
+                  validatePhone();
+                }}
                 onChange={(e) => changeHandler(e)}
               />
-              {validator.message(
-                "phone",
-                value.phone,
-                "required|phone"
+              {phoneError && (
+                <span className="errorMessage">{phoneError}</span>
               )}
+            </Grid>
+            <Grid item xs={12}>
+              <label>Date of Birth</label>
+              <DatePicker selected={user.dateOfBirth} onChange={(date) => handleDateChange(date)} />
             </Grid>
             <Grid item xs={12}>
               Language
               <RadioGroup
                 row
-                value={value.language}
+                user={user.language}
                 onChange={(e) => changeHandler(e)}
                 name="language"
               >
-                <FormControlLabel value="Vietnamese" control={<Radio />} label="Vietnamese" sx={{ paddingRight: 5 }} />
-                <FormControlLabel value="English" control={<Radio />} label="English" sx={{ paddingRight: 5 }} />
+                <FormControlLabel value="0" control={<Radio />} label="English" sx={{ paddingRight: 5 }} />
+                <FormControlLabel value="1" control={<Radio />} label="Vietnamese" sx={{ paddingRight: 5 }} />
               </RadioGroup>
             </Grid>
 
