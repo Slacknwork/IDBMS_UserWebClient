afe59f571bd3130066cd874766062241b34d0c75
galaxyyvfx <ksgalaxyy@gmail.com>
2023-11-29 00:29:56 +0700

    Fixed Decor Booking layout

--- app/[locale]/(MainLayout)/project/booking/decor/ClientLayout.js
+++ app/[locale]/(MainLayout)/project/booking/decor/ClientLayout.js
@@ -1,14 +1,14 @@
 "use client";
 
 import PageTitle from "/components/PageTitle";
-import DecorProjectBooking from "/components/ProjectBooking/Construction";
+import DecorProjectBooking from "/components/ProjectBooking/Decor";
 
 export default function ClientLayout({ children }) {
   return (
     <div style={{ backgroundColor: "#f0f0f0" }}>
       <PageTitle
-        pageTitle={"Construction Project Booking"}
-        pagesub={"Construction Project Booking"}
+        pageTitle={"Decor Project Booking"}
+        pagesub={"Decor Project Booking"}
       />
       <DecorProjectBooking>{children}</DecorProjectBooking>
     </div>
