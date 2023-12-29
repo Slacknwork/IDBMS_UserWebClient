61695b625c8650af6416d7ab5bab981021e89410
galaxyyvfx <ksgalaxyy@gmail.com>
2023-12-29 11:05:19 +0700

    Fix auth error

--- api/authenticationServices/index.js
+++ api/authenticationServices/index.js
@@ -1,25 +1,136 @@
-const API_BASE_URL = 'https://localhost:7062/api/Authentications';
+const API_BASE_URL = "https://localhost:7062/api/Authentications";
 
-const loginAdmin = async (request) => {
-    try {
-        const response = await fetch(`${API_BASE_URL}/admin/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(request),
-        });
+const registerUser = async (request) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
 
-        if (!response.ok) {
-            throw new Error('Login failed');
-        }
-
-        const admin = await response.json();
-        return admin;
-    } catch (error) {
-        console.error('Error logging in admin:', error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Registration failed");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
 };
 
-export { loginAdmin };
+const loginUser = async (request) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error logging in:", error);
+    throw error;
+  }
+};
+
+const updatePassword = async (request) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/update-password/${userId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+
+    if (!response.ok) {
+      throw new Error("Password update failed");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error updating password:", error);
+    throw error;
+  }
+};
+
+const logoutUser = async (email) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(email),
+    });
+
+    if (!response.ok) {
+      throw new Error("Logout failed");
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Error logging out:", error);
+    throw error;
+  }
+};
+
+const verifyUser = async (request) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/verify`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+
+    if (!response.ok) {
+      throw new Error("Verification failed");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error verifying user:", error);
+    throw error;
+  }
+};
+
+const resetPassword = async (request) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/reset-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+
+    if (!response.ok) {
+      throw new Error("Password reset failed");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error resetting password:", error);
+    throw error;
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  updatePassword,
+  logoutUser,
+  verifyUser,
+  resetPassword,
+};
