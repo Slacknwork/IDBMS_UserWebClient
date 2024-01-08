17983e7beaefae7b27c79a689bbe6e715ac29400
tkiet1808 <kietpttse161220@fpt.edu.vn>
2024-01-08 13:12:42 +0700

    add pagination for item list

--- constants/enums/projectTaskStatus.js
+++ constants/enums/projectTaskStatus.js
@@ -1,1 +1,27 @@
-export default ["Pending", "Confirmed", "Ongoing", "Done", "Cancelled"];
+const projectTaskStatusOptions = [
+    "Đang chờ",
+    "Xác nhận",
+    "Đang thực hiện",
+    "Hoàn thành",
+    "Hủy",
+];
+
+const projectTaskStatusIndex = {
+    Pending: 0,
+    Confirmed: 1,
+    Ongoing: 2,
+    Done: 3,
+    Cancelled: 4,
+};
+
+const projectTaskStatusOptionsEnglish = [
+    "Pending",
+    "Confirmed",
+    "Ongoing",
+    "Done",
+    "Cancelled",
+];
+
+export { projectTaskStatusIndex, projectTaskStatusOptionsEnglish };
+
+export default projectTaskStatusOptions;
