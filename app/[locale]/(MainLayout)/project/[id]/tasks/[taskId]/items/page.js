72ee3ce78ea91e689cecc558a45c4781a1cfbf70
galaxyyvfx <ksgalaxyy@gmail.com>
2024-01-16 12:47:49 +0700

    Refactor task items page

--- app/[locale]/(MainLayout)/project/[id]/tasks/[taskId]/items/page.js
+++ app/[locale]/(MainLayout)/project/[id]/tasks/[taskId]/items/page.js
@@ -12,7 +12,9 @@ import { getAllInteriorItemCategories } from "/services/interiorItemCategoryServ
 
 import Search from "/components/Shared/Search";
 import Pagination from "/components/Shared/Pagination";
-import projectTaskStatusOptions, { projectTaskStatusOptionsEnglish } from "/constants/enums/projectTaskStatus";
+import projectTaskStatusOptions, {
+  projectTaskStatusOptionsEnglish,
+} from "/constants/enums/projectTaskStatus";
 import { useTranslations } from "next-intl";
 
 export default function ItemsPage() {
@@ -32,7 +34,12 @@ export default function ItemsPage() {
   const searchParams = useSearchParams();
   const t = useTranslations("ProjectDetails_Item");
   const e = useTranslations("Error");
-  const language = params?.locale === "en-US" ? "english" : params?.locale === "vi-VN" ? "vietnamese" : "";
+  const language =
+    params?.locale === "en-US"
+      ? "english"
+      : params?.locale === "vi-VN"
+      ? "vietnamese"
+      : "";
 
   // FETCH DATA
   const [loading, setLoading] = useState(true);
@@ -40,37 +47,33 @@ export default function ItemsPage() {
   const [count, setCount] = useState(0);
 
   // FETCH DATA
-  const fetchDataFromApi = async () => {
-    const fetchItems = async () => {
-      const projectId = params.id;
-      const taskId = params.taskId;
-      const search = searchParams.get(searchQuery) || "";
-      const categoryId = searchParams.get(categoryQuery) || "";
-      const status = searchParams.get(statusQuery)
-        ? parseInt(searchParams.get(statusQuery))
-        : "";
-      const page = parseInt(searchParams.get(pageQuery)) || defaultPage;
-      const pageSize =
-        parseInt(searchParams.get(pageSizeQuery)) || defaultPageSize;
+  const fetchItems = async () => {
+    const projectId = params.id;
+    const taskId = params.taskId;
+    const search = searchParams.get(searchQuery) ?? "";
+    const categoryId = searchParams.get(categoryQuery) ?? "";
+    const status = searchParams.get(statusQuery) ?? "";
+    const page = searchParams.get(pageQuery) ?? defaultPage;
+    const pageSize = searchParams.get(pageSizeQuery) ?? defaultPageSize;
 
-      try {
-        const response = await getItemInTasksByTaskId({
-          projectId,
-          taskId,
-          search,
-          categoryId,
-          status,
-          page,
-          pageSize,
-        });
-        console.log(response);
-        setItems(response?.list ?? []);
-        setCount(response?.totalPage ?? 0);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        toast.error("Lỗi nạp dữ liệu từ hệ thống");
-      }
-    };
+    try {
+      const response = await getItemInTasksByTaskId({
+        projectId,
+        taskId,
+        search,
+        categoryId,
+        status,
+        page,
+        pageSize,
+      });
+      setItems(response?.list ?? []);
+      setCount(response?.totalPage ?? 0);
+    } catch (error) {
+      toast.error("Lỗi nạp dữ liệu từ hệ thống");
+    }
+  };
+  const fetchDataFromApi = async () => {
+    setLoading(true);
     await Promise.all([fetchItems()]);
     setLoading(false);
   };
@@ -82,21 +85,17 @@ export default function ItemsPage() {
   // ITEM CATEGORIES
   const [itemCategories, setItemCategories] = useState([]);
 
+  const fetchCategories = async () => {
+    try {
+      const response = await getAllInteriorItemCategories();
+      setItemCategories(response.list);
+    } catch (error) {
+      toast.error(e("FetchError"));
+    }
+  };
   // FETCH OPTIONS
   const fetchOptionsFromApi = async () => {
-    setLoading(true);
-    const fetchCategories = async () => {
-      try {
-        const response = await getAllInteriorItemCategories();
-        console.log(response);
-        setItemCategories(response.list);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        toast.error(e("FetchError"));
-      }
-    };
     await Promise.all([fetchCategories()]);
-    setLoading(false);
   };
 
   useEffect(() => {
@@ -104,11 +103,12 @@ export default function ItemsPage() {
   }, []);
 
   return (
-    <div
-      style={{
-        minHeight: "35rem",
-      }}
-    >
+    <div className="container">
+      <div className="row">
+        <div className="col col-lg-12 col-12 mb-3">
+          <h3 style={{ fontSize: 24 }}>Items</h3>
+        </div>
+      </div>
       <div className="row">
         <div className="col col-lg-6 col-12 mb-4">
           <Search placeholder={t("SearchItems")}></Search>
@@ -126,7 +126,9 @@ export default function ItemsPage() {
                   >
                     {itemCategories.map((category) => (
                       <option value={category.id} key={category.id}>
-                        {language === "english" ? category.englishName : category.name}
+                        {language === "english"
+                          ? category.englishName
+                          : category.name}
                       </option>
                     ))}
                   </select>
@@ -142,7 +144,9 @@ export default function ItemsPage() {
                   >
                     {projectTaskStatusOptions.map((status, index) => (
                       <option key={status} value={index}>
-                        {language === "english" ? projectTaskStatusOptionsEnglish[index] : status}
+                        {language === "english"
+                          ? projectTaskStatusOptionsEnglish[index]
+                          : status}
                       </option>
                     ))}
                   </select>
@@ -152,95 +156,97 @@ export default function ItemsPage() {
           </form>
         </div>
       </div>
-
-      {loading ? (
-        <Stack sx={{ height: "30rem" }}>
-          <CircularProgress
-            sx={{ m: "auto", color: "#CAAD06" }}
-            size="3rem"
-          ></CircularProgress>
-        </Stack>
-      ) : (
-        <table className="table table-striped table-hover">
-          <thead
-            className="shadow-sm"
-            style={{ position: "sticky", top: 0, zIndex: 1 }}
-          >
-            <tr>
-              <th scope="col" style={{ width: "12rem" }}>
-              {t("Image")}
-              </th>
-              <th scope="col">{t("Name")}</th>
-              <th scope="col">{t("Category")}</th>
-              <th scope="col">{t("Quantity")}</th>
-              <th scope="col" style={{ width: "10rem" }}>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {items &&
-              items.map((item) => (
-                <tr key={item.id}>
-                  <td className="align-middle">
-                    <div className="shop-img">
-                      <Image
-                        src={item && item.interiorItem?.imageUrl}
-                        alt=""
-                        width={500}
-                        height={500}
-                        style={{
-                          width: "6rem",
-                          height: "6rem",
-                          objectFit: "cover",
-                        }}
-                        unoptimized={true}
-                      />
-                    </div>
-                  </td>
-                  <td className="align-middle">
-                    {
-                      (() => {
+      <div className="row">
+        <div className="col col-lg-12 col-12" style={{ height: "30rem" }}>
+          {loading ? (
+            <Stack sx={{ height: "30rem" }}>
+              <CircularProgress
+                sx={{ m: "auto", color: "#CAAD06" }}
+                size="3rem"
+              ></CircularProgress>
+            </Stack>
+          ) : items && items.length > 0 ? (
+            <table className="table table-striped table-hover">
+              <thead
+                className="shadow-sm"
+                style={{ position: "sticky", top: 0, zIndex: 1 }}
+              >
+                <tr>
+                  <th scope="col" style={{ width: "12rem" }}>
+                    {t("Image")}
+                  </th>
+                  <th scope="col">{t("Name")}</th>
+                  <th scope="col">{t("Category")}</th>
+                  <th scope="col">{t("Quantity")}</th>
+                  <th scope="col" style={{ width: "10rem" }}></th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map((item) => (
+                  <tr key={item.id}>
+                    <td className="align-middle">
+                      <div className="shop-img">
+                        <Image
+                          src={item && item.interiorItem?.imageUrl}
+                          alt=""
+                          width={500}
+                          height={500}
+                          style={{
+                            width: "6rem",
+                            height: "6rem",
+                            objectFit: "cover",
+                          }}
+                          unoptimized={true}
+                        />
+                      </div>
+                    </td>
+                    <td className="align-middle">
+                      {(() => {
                         if (language === "english") {
                           return item?.interiorItem?.englishName;
                         } else if (language === "vietnamese") {
                           return item?.interiorItem?.name;
                         } else {
-                          return '';
+                          return "";
                         }
-                      })()
-                    }
-                  </td>
-                  <td className="align-middle">
-                    {
-                      (() => {
+                      })()}
+                    </td>
+                    <td className="align-middle">
+                      {(() => {
                         if (language === "english") {
-                          return item?.interiorItem?.interiorItemCategory?.englishName;
+                          return item?.interiorItem?.interiorItemCategory
+                            ?.englishName;
                         } else if (language === "vietnamese") {
                           return item?.interiorItem?.interiorItemCategory?.name;
                         } else {
-                          return '';
+                          return "";
                         }
-                      })()
-                    }
-                  </td>
-                  <td className="align-middle">{item && item.quantity}</td>
-                  <td className="align-middle m-0">
-                    <div className="d-flex">
-                      <Link
-                        href={`/items/${item?.interiorItem.id}`}
-                        className="theme-btn m-1"
-                        style={{ width: "6rem", zIndex: 0 }}
-                      >
-                        {t("Details")}
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      )}
-      <Pagination count={count}></Pagination>
+                      })()}
+                    </td>
+                    <td className="align-middle">{item && item.quantity}</td>
+                    <td className="align-middle m-0">
+                      <div className="d-flex">
+                        <Link
+                          href={`/items/${item?.interiorItem.id}`}
+                          className="theme-btn m-1"
+                          style={{ width: "6rem", zIndex: 0 }}
+                        >
+                          {t("Details")}
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <Stack sx={{ height: "100%" }}>
+              <p style={{ margin: "auto", textAlign: "center" }}>No data.</p>
+            </Stack>
+          )}
+          <Pagination count={count}></Pagination>
+        </div>
+      </div>
     </div>
   );
 }
