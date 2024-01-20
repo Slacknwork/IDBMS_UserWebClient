b448c9b2d9b729bc8e0533dd0f0287c36865ca62
galaxyyvfx <ksgalaxyy@gmail.com>
2024-01-20 14:39:49 +0700

    Implement map

--- app/[locale]/(MainLayout)/about/page.js
+++ app/[locale]/(MainLayout)/about/page.js
@@ -15,6 +15,19 @@ export default function AboutPage() {
       <Overview />
       <Projects />
       <Contact />
+      <div style={{ width: "100%" }}>
+        <iframe
+          width="100%"
+          height="600"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=721%20Phan%20Van%20Tri+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          <a href="https://www.maps.ie/population/">Population Estimator map</a>
+        </iframe>
+      </div>
     </div>
   );
 }
